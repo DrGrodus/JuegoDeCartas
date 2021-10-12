d04e6725c73172349748c5d06cc786269e746b1c
DrGrodus <ebermd00@gmail.com>
2021-10-12 15:02:54 -0500

    Clase Deck y Card agregada. La clase Deck ahora tiene 2 atributos

--- JuegoDeCartas/src/juegodecartas/JuegoDeCartas.java
+++ JuegoDeCartas/src/juegodecartas/JuegoDeCartas.java
@@ -1,11 +1,20 @@
 package juegodecartas;
-
+import java.util.*;
 /**
  *
  * @author Eber MD
  */
-public class JuegoDeCartas {
+class Deck{ // clase baraja
+        Queue<Integer> barajaCentral = new LinkedList<>(); //de aqui se repartirán las cartas
+        List<Integer> barajaPersonal = new LinkedList<>(); // aqui se guarda la baraja de cada jugador
+}
 
+class Card{ // clase carta
+    
+}
+    
+
+public class JuegoDeCartas {
     public static void main(String[] args) {
         
     }
