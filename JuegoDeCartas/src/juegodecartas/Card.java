bec09825772d0894f454edde13400aae3420ac4d
DrGrodus <ebermd00@gmail.com>
2021-10-14 13:56:30 -0500

    Documentacion interna

--- JuegoDeCartas/src/juegodecartas/Card.java
+++ JuegoDeCartas/src/juegodecartas/Card.java
@@ -8,17 +8,17 @@ public class Card {// clase carta
     private String color; // rojo o negro.
     private String valor; // del 2 al 10, A, J, Q o K.
 
-    public Card(String palo, String color, String valor){
+    public Card(String palo, String color, String valor){ // esto es un constructor, es con esto que se inicializan los objetos de tipo de dato complejo
         this.palo = palo;
         this.color = color;
         this.valor = valor;
     }
     
-    public String getPalo() {
+    public String getPalo() { // con este se consigue el valor en este caso de palo
         return palo;
     }
 
-    public void setPalo(String palo) {
+    public void setPalo(String palo) { // y por supuesto, con este se establece el valor, de igual forma, aquí es palo
         this.palo = palo;
     }
 
--- JuegoDeCartas/src/juegodecartas/Deck.java
+++ JuegoDeCartas/src/juegodecartas/Deck.java
@@ -20,41 +20,41 @@ public class Deck {
             this.card = card;
         }
         
-        public Integer nCards(){
+        public Integer nCards(){ // Metodo para conocer la cantidad de cartas que hay en la mesa, especialmente util en llamados de otros metodos
             return numCard;
         }
         
         public String shuffle(){
             Collections.shuffle(card);
-            return "Se mezcló el Deck";
+            return "Se mezclo el Deck"; // se mezclan las cartas
         }
         
         public String head(Integer numcard){
             // se borra la de hasta arriba
-            card.get(0);
-            numCard--;
+            card.get(0); // tomamos la carta de la parte superior
+            numCard--; // inmediatamente restamos al total de cartas
             String respuesta = "Tarjeta: " + " color: " + card.get(0).getColor() + " palo: "+card.get(0).getPalo() + " valor: " + card.get(0).getValor() + "\n" + "Quedan: " + numcard;
-            return respuesta;
+            return respuesta; // devolvemos la respuesta
             
         }
         
         public String pick(Integer numcard){
             // se borra alguna otra
-            int dato = (int)(Math.random()*52);
-            card.get(dato);
+            int dato = (int)(Math.random()*52); // esta operacion devuelve valores enteros no mayores a 52
+            card.get(dato); // cual sea el dato, sera la tarjeta que tomara
             numCard--;
             String respuesta = "Tarjeta: " + " color: " + card.get(dato).getColor() + " palo: " + card.get(dato).getPalo() + " valor: " + card.get(dato).getValor() + "\n" + "Quedan: " + numcard;
-            return respuesta;
+            return respuesta; // se devuelve la respuesta
         }
         
         public String hand(Integer numcard){
-            String[] mano = new String[5]; String capturador = "";
-            int dato = 0;
+            String[] mano = new String[5]; String capturador = ""; // tenemos un arreglo de cadenas que guardará cada tarjeta como elemento de String y la otra cadena será para entregar todos los valores de una tarjeta a la posicion del arreglo
+            int dato = 0; // esto es simplemente capta el numero aleatorio de la tarjeta, igual que con el metodo pick
             for(int i = 0; i < 5; i++){
                 dato = (int) (Math.random()*52);
                 card.get(dato);
                 capturador = "Tarjeta: ";
-                capturador = capturador.concat(" color: "+card.get(dato).getColor()+" ");
+                capturador = capturador.concat(" color: "+card.get(dato).getColor()+" "); // utilizamos el metodo concat para juntar los atributos de la tarjeta y poder prresentarlos con la cadena respuesta
                 capturador = capturador.concat(" palo: "+card.get(dato).getPalo()+" ");
                 capturador = capturador.concat(" valor: "+card.get(dato).getValor()+" \n");
                 mano[i] = capturador;
