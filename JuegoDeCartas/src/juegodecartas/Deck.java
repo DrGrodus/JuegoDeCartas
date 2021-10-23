f03e44d5a951a5762586109972d5b6931dcc4008
DrGrodus <ebermd00@gmail.com>
2021-10-22 22:33:50 -0500

    Se agregó el método ShowMenu y se hicieron correciones de código

--- JuegoDeCartas/src/juegodecartas/Deck.java
+++ JuegoDeCartas/src/juegodecartas/Deck.java
@@ -20,50 +20,54 @@ public class Deck {
             this.card = card;
         }
         
-        public Integer nCards(){ // Metodo para conocer la cantidad de cartas que hay en la mesa, especialmente util en llamados de otros metodos
-            return numCard;
-        }
-        
         public String shuffle(){
             Collections.shuffle(card);
             return "Se mezclo el Deck"; // se mezclan las cartas
         }
         
-        public String head(Integer numcard){
+        public String head(){
             // se borra la de hasta arriba
             card.get(0); // tomamos la carta de la parte superior
-            numCard--; // inmediatamente restamos al total de cartas
-            String respuesta = "Tarjeta: " + " color: " + card.get(0).getColor() + " palo: "+card.get(0).getPalo() + " valor: " + card.get(0).getValor() + "\n" + "Quedan: " + numcard;
+            numCard--; // restamos al total de cartas
+            String respuesta = "Tarjeta: " + " color: " + card.get(0).getColor() + " palo: "+card.get(0).getPalo() + " valor: " + card.get(0).getValor() + "\n" + "Quedan: " + numCard;
             card.remove(0);
+            
             return respuesta; // devolvemos la respuesta
             
         }
         
-        public String pick(Integer numcard){
+        public String pick(){
             // se borra alguna otra
-            int dato = (int)(Math.random()*52); // esta operacion devuelve valores enteros no mayores a 52
+            int dato = (int)(Math.random()*numCard); // esta operacion devuelve valores enteros no mayores a 52
             card.get(dato); // cual sea el dato, sera la tarjeta que tomara
             numCard--;
-            String respuesta = "Tarjeta: " + " color: " + card.get(dato).getColor() + " palo: " + card.get(dato).getPalo() + " valor: " + card.get(dato).getValor() + "\n" + "Quedan: " + numcard;
+            String respuesta = "Tarjeta: " + " color: " + card.get(dato).getColor() + " palo: " + card.get(dato).getPalo() + " valor: " + card.get(dato).getValor() + "\n" + "Quedan: " + numCard;
             card.remove(dato);
+            
             return respuesta; // se devuelve la respuesta
         }
         
-        public String hand(Integer numcard){
+        public String hand(){
             String[] mano = new String[5]; String capturador = ""; // tenemos un arreglo de cadenas que guardará cada tarjeta como elemento de String y la otra cadena será para entregar todos los valores de una tarjeta a la posicion del arreglo
             int dato = 0; // esto es simplemente capta el numero aleatorio de la tarjeta, igual que con el metodo pick
-            for(int i = 0; i < 5; i++){
-                dato = (int) (Math.random()*52);
-                card.get(dato);
-                capturador = "Tarjeta: ";
-                capturador = capturador.concat(" color: "+card.get(dato).getColor()+" "); // utilizamos el metodo concat para juntar los atributos de la tarjeta y poder prresentarlos con la cadena respuesta
-                capturador = capturador.concat(" palo: "+card.get(dato).getPalo()+" ");
-                capturador = capturador.concat(" valor: "+card.get(dato).getValor()+" \n");
-                mano[i] = capturador;
-                capturador = "";
-                card.remove(dato);
+            try{
+                for(int i = 0; i < 5; i++){
+                    dato = (int) (Math.random()*numCard);
+                    card.get(dato);
+                    capturador = "Tarjeta: ";
+                    capturador = capturador.concat(" color: "+card.get(dato).getColor()+" "); // utilizamos el metodo concat para juntar los atributos de la tarjeta y poder prresentarlos con la cadena respuesta
+                    capturador = capturador.concat(" palo: "+card.get(dato).getPalo()+" ");
+                    capturador = capturador.concat(" valor: "+card.get(dato).getValor()+" \n");
+                    mano[i] = capturador;
+                    capturador = "";
+                    card.remove(dato);
+                }
+            } catch(IndexOutOfBoundsException e){
+                System.out.println(e);
             }
-            String respuesta = ""; respuesta = respuesta.concat(Arrays.toString(mano)+"Quedan: " + numcard);
+            numCard = numCard-5; // se reduce 5 veces el contador 
+            numCard = (numCard < 5) ? numCard = 0 : Math.abs(numCard); // se determina si el valor puede ser negativo o no
+            String respuesta = ""; respuesta = respuesta.concat(Arrays.toString(mano)+"Quedan: " + numCard);
             return respuesta;
         }
 }
--- JuegoDeCartas/src/juegodecartas/JuegoDeCartas.java
+++ JuegoDeCartas/src/juegodecartas/JuegoDeCartas.java
@@ -6,26 +6,24 @@ import java.util.*;
  */ 
 
 public class JuegoDeCartas {
+    public static Deck deck = new Deck();
+    
     public static void main(String[] args) {
         String[] color = {"rojo", "negro"};
         String[] palo = {"Trebol", "Diamante", "Pica", "Corazon"};
         String[] valor = {"2", "3", "4", "5", "6", "7", "8", "9", "10","A", "J", "Q", "K"};
         Integer cont = 1;
-        Deck deck = new Deck();
+        String accion = "";
+        
         deck.setCard(new ArrayList<Card>());
         for(int i = 0; i < palo.length; i++){
             for(int j = 0; j < valor.length; j++){
                 Card card = new Card(palo[i], getColor(palo[i]), valor[j]); 
                 deck.getCard().add(card);
-                //System.out.println(cont+"-Palo: "+palo[i]+" "+ "valor: "+ valor[j]+" color: "+getColor(palo[i]));
                 cont++;
             }
         }
-        System.out.println("el tamanho del deck es: " + deck.getCard().size()+"\n");
-        System.out.println("Mezclar Deck\n" + deck.shuffle() +"\n");
-        System.out.println(deck.head(deck.nCards())+"\n");
-        System.out.println(deck.pick(deck.nCards())+"\n");
-        System.out.println(deck.hand(deck.nCards()));
+        ShowMenu();
     }
     public static String getColor(String palo){
         if(palo.toLowerCase().equals("pica") || palo.toLowerCase().equals("trebol")){
@@ -35,5 +33,46 @@ public class JuegoDeCartas {
             return "rojo"; 
         }
         return null;
+    }
+    
+    public static void ShowMenu(){
+        Scanner lector = new Scanner(System.in);
+        int resp = 1; // variable de control para el switch.
+        while(resp != 0){ // primero verifica sí se cumple la condición y después continua.
+            System.out.println("Bienvenido a Póker!" 
+                    + "\n" + "el tamanho del deck es: " + deck.getCard().size()
+                    + "\n" + "Selecciona una opción: " 
+                    + "\n" + "1 - Mezclar Deck"
+                    + "\n" + "2 - Sacar una carta"
+                    + "\n" + "3 - Sacar carta al azar"
+                    + "\n" + "4 - Generar una mano de 5 cartas"
+                    + "\n" + "0 - Salir");
+            try{
+                resp = lector.nextInt();
+                switch(resp){
+                    case 1:
+                        System.out.println(deck.shuffle() +"\n");
+                        break;
+                    case 2:
+                        System.out.println(deck.head() + "\n");
+                        break;
+                    case 3:
+                        System.out.println(deck.pick() + "\n");
+                        break;
+                    case 4:
+                        System.out.println(deck.hand() + "\n");
+                        break;
+                    case 0:
+                        System.out.println("Saliendo...");
+                        break;
+                    default:
+                        System.out.println("La opción indicada no es válida, por favor selecciona una de las siguientes: " + "\n");
+                        break;
+                }
+            }catch(InputMismatchException e){ // este tipo de error logra atrapar cuando se introduce un caracter no numérico.
+                 System.out.println("Por favor solo introduce numeros" + "\n");
+                 lector.nextLine(); // limpia el buffer, borrando el contenido, lo que permite ser llenado nuevamente.
+            }
+        }
     }
 }
